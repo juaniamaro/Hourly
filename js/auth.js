aecd63d309f006c9d4c71cc5fe5dd73cc50a5f7f
Juani Amaro González <48890906+juaniamaro@users.noreply.github.com>
2026-01-21 13:20:59 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,10 +1,10 @@
-const supabaseUrl = 'https://pfbfpifwzuicwztepvwh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmYmZwaWZ3enVpY3d6dGVwdndoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5MTk4OTcsImV4cCI6MjA4NDQ5NTg5N30.7At1iAHw0DTsx0qe_J_IsQy7E-JXc0G2UB41MMD9xRM';
+const SUPABASE_URL = 'https://pfbfpifwzuicwztepvwh.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmYmZwaWZ3enVpY3d6dGVwdndoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5MTk4OTcsImV4cCI6MjA4NDQ5NTg5N30.7At1iAHw0DTsx0qe_J_IsQy7E-JXc0G2UB41MMD9xRM';
 
 // Crear cliente Supabase
 const supabaseClient = supabase.createClient(
-  supabaseUrl,
-  supabaseKey
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
 
 // LOGIN
