70febd7f7fd40d24ad68f80cd1e7a1fe7e4f72f6
Juani Amaro González <48890906+juaniamaro@users.noreply.github.com>
2026-01-21 13:01:08 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,33 @@
+const supabaseUrl = 'https://TU_PROYECTO.supabase.co';
+const supabaseKey = 'TU_PUBLIC_ANON_KEY';
+
+const supabase = supabaseJs.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+
+/* LOGIN */
+async function login(email, password){
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if(error){
+    alert('Credenciales incorrectas');
+    return;
+  }
+
+  // Login correcto → dashboard
+  window.location.href = 'dashboard.html';
+}
+
+/* PROTEGER PÁGINAS */
+async function checkSession(){
+  const { data:{ session } } = await supabase.auth.getSession();
+
+  if(!session){
+    window.location.href = 'index.html';
+  }
+}
 
