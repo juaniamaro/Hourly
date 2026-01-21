404313a255fa93d3139ee607b0eed1b93a5106fb
Juani Amaro González <48890906+juaniamaro@users.noreply.github.com>
2026-01-21 13:07:29 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
-const supabaseUrl = 'https://TU_PROYECTO.supabase.co';
-const supabaseKey = 'TU_PUBLIC_ANON_KEY';
+const supabaseUrl = 'https://pfbfpifwzuicwztepvwh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmYmZwaWZ3enVpY3d6dGVwdndoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5MTk4OTcsImV4cCI6MjA4NDQ5NTg5N30.7At1iAHw0DTsx0qe_J_IsQy7E-JXc0G2UB41MMD9xRM';
 
 const supabase = supabaseJs.createClient(
   supabaseUrl,
