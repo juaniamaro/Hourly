d552627c51795b7890cfdc9b0890949e847ea024
Juani Amaro González <48890906+juaniamaro@users.noreply.github.com>
2026-01-21 13:16:50 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,33 +1,26 @@
 const supabaseUrl = 'https://pfbfpifwzuicwztepvwh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmYmZwaWZ3enVpY3d6dGVwdndoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5MTk4OTcsImV4cCI6MjA4NDQ5NTg5N30.7At1iAHw0DTsx0qe_J_IsQy7E-JXc0G2UB41MMD9xRM';
 
-const supabase = supabaseJs.createClient(
-  supabaseUrl,
-  supabaseKey
+// Crear cliente Supabase
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
 
-/* LOGIN */
+// LOGIN
 async function login(email, password){
-  const { data, error } = await supabase.auth.signInWithPassword({
+  const { error } = await supabaseClient.auth.signInWithPassword({
     email,
     password
   });
 
   if(error){
     alert('Credenciales incorrectas');
+    console.error(error);
     return;
   }
 
-  // Login correcto → dashboard
+  // Login correcto
   window.location.href = 'dashboard.html';
-}
-
-/* PROTEGER PÁGINAS */
-async function checkSession(){
-  const { data:{ session } } = await supabase.auth.getSession();
-
-  if(!session){
-    window.location.href = 'index.html';
-  }
 }
 
